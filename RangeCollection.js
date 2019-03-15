f1a1492652fb00166149daa15b1f99dee9968625
Felonius Gru <new-comet-dev@outlook.com>
2019-03-15 16:03:00 +0400

    separate class file and running file

--- RangeCollection.js
+++ RangeCollection.js
@@ -6,7 +6,7 @@
  * RangeCollection class
  * NOTE: Feel free to add any extra member variables/functions you like.
  */
-class RangeCollection {
+export class RangeCollection {
     /**
      * Adds a range to the collection
      * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
@@ -30,46 +30,3 @@ class RangeCollection {
         // TODO: implement this
     }
 }
-
-// Example run
-const rc = new RangeCollection();
-
-rc.add([1, 5]);
-rc.print();
-// Should display: [1, 5)
-
-rc.add([10, 20]);
-rc.print();
-// Should display: [1, 5) [10, 20)
-
-rc.add([20, 20]);
-rc.print();
-// Should display: [1, 5) [10, 20)
-
-rc.add([20, 21]);
-rc.print();
-// Should display: [1, 5) [10, 21)
-
-rc.add([2, 4]);
-rc.print();
-// Should display: [1, 5) [10, 21)
-
-rc.add([3, 8]);
-rc.print();
-// Should display: [1, 8) [10, 21)
-
-rc.remove([10, 10]);
-rc.print();
-// Should display: [1, 8) [10, 21)
-
-rc.remove([10, 11]);
-rc.print();
-// Should display: [1, 8) [11, 21)
-
-rc.remove([15, 17]);
-rc.print();
-// Should display: [1, 8) [11, 15) [17, 21)
-
-rc.remove([3, 19]);
-rc.print();
-// Should display: [1, 3) [19, 21)
